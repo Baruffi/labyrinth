7f20ae46b69bc1a30696654fe5fe33e7ded3e0c6
Raphael Antunes Baruffi <42071700+Baruffi@users.noreply.github.com>
2021-06-10 22:24:30 -0300

    programada visao inicial do robo para paredes e objetivos, assim como tweaks para 'deslizar pelas paredes' e andar automaticamente até o objetivo quando encontrá-lo

--- classes/Robo.py
+++ classes/Robo.py
@@ -1,4 +1,5 @@
 from ursina import *
+from ursina.hit_info import HitInfo
 
 
 class Robo(Entity):
@@ -12,26 +13,109 @@ class Robo(Entity):
         self.collider = 'box'
         self.position = position
         self.velocidade = 10
-        self.direcao = None
-        self.traverse_target = scene
+        self.sentido = 1
+        self.direcao = Vec3(0, 0, 0).normalized()
+        self.is_moving = False
+        self.mapa_construido = set()
+        self.objetivo = None
+        self.posicao_objetivo = None
+        self.override_human = False
 
     def reset(self):
-        pass
+        self.direcao = Vec3(0, 0, 0).normalized()
+        self.is_moving = False
+        self.mapa_construido = set()
+        self.objetivo = None
+        self.posicao_objetivo = None
+        self.override_human = False
+
+    def getOrigin(self):
+        return self.world_position + Vec3(0, self.scale_y / 2, 0)
+
+    def see(self):
+        if self.objetivo:
+            hit_info = boxcast(self.getOrigin(), ignore=(self, self.objetivo, ),
+                               thickness=(2.5, 2.5), distance=2.5, debug=True)
+
+            return hit_info
+
+    def findObjective(self):
+        if self.objetivo and not self.posicao_objetivo:
+            hit_info = boxcast(self.getOrigin(), ignore=(
+                self,), traverse_target=self.objetivo, thickness=(2.5, 2.5), distance=2.5, debug=True)
+
+            if hit_info.hit:
+                self.posicao_objetivo = hit_info.world_point
+                self.is_moving = True
+                self.override_human = True
+
+    def moveToObjective(self):
+        if self.posicao_objetivo:
+            self.direcao = Vec3(
+                self.down * (self.world_y - self.posicao_objetivo.y)
+                + self.left * (self.world_x - self.posicao_objetivo.x)
+            ).normalized()
+
+    def memorizeHitinfo(self, hit_info: HitInfo):
+        self.mapa_construido.union(hit_info.hits)
+
+    def canMove(self):
+        hit_info = raycast(self.getOrigin(), self.direcao, ignore=(
+            self,), distance=self.scale_y / 2)
+
+        return not hit_info.hit
 
     def move(self):
         self.position += self.direcao * time.dt * self.velocidade
 
-    def update(self):
-        self.direcao = None
+    def isMoving(self):
+        if not self.override_human:
+            if held_keys['w'] or held_keys['s'] or held_keys['d'] or held_keys['a']:
+                self.is_moving = True
+            else:
+                self.is_moving = False
 
-        self.direcao = Vec3(
-            self.up * (held_keys['w'] - held_keys['s'])
-            + self.right * (held_keys['d'] - held_keys['a'])
-        ).normalized()
+        return self.is_moving
 
-        origin = self.world_position + Vec3(0, self.scale_y / 2, 0)
-        hit_info = raycast(origin, self.direcao, ignore=(
+    def updateDirection(self):
+        if held_keys['w'] or held_keys['s'] or held_keys['d'] or held_keys['a']:
+            self.direcao = Vec3(
+                self.up * (held_keys['w'] - held_keys['s'])
+                + self.right * (held_keys['d'] - held_keys['a'])
+            ).normalized()
+
+        self.direcao = self.direcao * self.sentido
+
+    def tweakDirection(self):
+        hit_info = raycast(self.getOrigin(), self.direcao, ignore=(
             self,), distance=self.scale_y / 2)
 
-        if not hit_info.hit:
-            self.move()
+        self.direcao = Vec3(
+            self.up * (hit_info.world_normal.x)
+            + self.right * (hit_info.world_normal.y)
+        ).normalized()
+
+    def update(self):
+        self.findObjective()
+        self.moveToObjective()
+
+        visao = self.see()
+
+        if visao:
+            self.memorizeHitinfo(visao)
+
+        self.updateDirection()
+
+        self.move_cycle(50)
+
+    def move_cycle(self, depth: int):
+        if depth == 0:
+            self.sentido = - self.sentido
+            return
+
+        if self.isMoving():
+            if self.canMove():
+                self.move()
+            else:
+                self.tweakDirection()
+                self.move_cycle(depth - 1)
--- game.py
+++ game.py
@@ -17,9 +17,9 @@ window.fps_counter.enabled = True
 camera.orthographic = True
 camera.fov = 24
 
-robo = Robo(0, 0)
-labirinto = Labirinto()
 mapa = Mapa()
+labirinto = Labirinto()
+robo = Robo(0, 0)
 
 
 def reset():
@@ -33,9 +33,13 @@ def reset():
     inicio_x, inicio_y = mapa.inicio
 
     robo.position = inicio_x + .5, inicio_y + .5
+    robo.objetivo = labirinto.fim
 
 
 def update():
+    if held_keys['r']:
+        reset()
+
     if robo.intersects(labirinto).hit:
         if robo.intersects(labirinto.fim).hit:
             reset()
