f53b9612d8bc4e3c0b931ad169bdc289c068ff53
Raphael Antunes Baruffi <42071700+Baruffi@users.noreply.github.com>
2021-06-12 23:20:57 -0300

    simplificacoes e melhorias para encontro de objetivo e navegar o historico

--- classes/Robo.py
+++ classes/Robo.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 from ursina import *
 from ursina.hit_info import HitInfo
@@ -10,7 +10,7 @@ quad = load_model('quad')
 
 class Robo(Entity):
 
-    def __init__(self, caminho: Caminho, espera: float = 3, velocidade: int = 10):
+    def __init__(self, caminho: Caminho, espera: float = 3, velocidade: int = 5):
         super().__init__()
         self.model = 'quad'
         self.color = color.blue
@@ -23,44 +23,51 @@ class Robo(Entity):
         self.velocidade = velocidade
 
         self.direcao = Vec3(0, 0, 0).normalized()
-        self.is_moving = False
+        self.delta = 0
 
         self.objetivo = None
         self.posicao_objetivo = None
-        self.override_human = False
 
         self.caminho = caminho
-        self.history: List[str] = list()
+        self.historia: List[Tuple[str, float]] = list()
 
     def reset(self):
         self.espera = self.espera_padrao
 
         self.direcao = Vec3(0, 0, 0).normalized()
-        self.is_moving = False
+        self.delta = 0
 
         self.objetivo = None
         self.posicao_objetivo = None
-        self.override_human = False
 
         self.caminho.reset()
-        self.history = list()
+        self.historia = list()
 
     def see(self):
         if self.objetivo:
-            hit_info = boxcast(self.world_position, ignore=(self, self.objetivo, ),
-                               thickness=(2, 2), distance=2)
+            hit_info = boxcast(self.world_position, ignore=(
+                self, self.objetivo, ), thickness=(2, 2), distance=2)
 
             return hit_info
 
-    def findObjective(self):
+    def find_objective(self):
         if self.objetivo and not self.posicao_objetivo:
-            hit_info = boxcast(self.world_position, ignore=(
-                self,), traverse_target=self.objetivo, thickness=(2, 2), distance=2)
+            hit_info = self.lookup(self.up)
+
+            if not hit_info.hit:
+                hit_info = self.lookup(self.down)
+            if not hit_info.hit:
+                hit_info = self.lookup(self.left)
+            if not hit_info.hit:
+                hit_info = self.lookup(self.right)
 
             if hit_info.hit:
                 self.posicao_objetivo = hit_info.world_point
                 self.is_moving = True
                 self.override_human = True
+
+    def lookup(self, direcao: Vec3):
+        return boxcast(self.world_position, direcao, traverse_target=self.objetivo, thickness=(self.scale_x * .8, self.scale_y * .8), distance=1)
 
     def canMove(self):
         hit_info = boxcast(self.world_position, self.direcao, ignore=(
@@ -69,74 +76,20 @@ class Robo(Entity):
         return not hit_info.hit
 
     def move(self):
-        self.position += self.direcao * time.dt * self.velocidade
+        self.position += self.direcao * self.delta * self.velocidade
 
-    def isMoving(self):
-        if not self.override_human:
-            if held_keys['w'] or held_keys['s'] or held_keys['d'] or held_keys['a']:
-                self.is_moving = True
-            else:
-                self.is_moving = False
-
-        return self.is_moving
-
-    def getRear(self):
+    def get_rear(self):
         direcao_x = (self.world_x - (self.direcao.x / 2)) // 1
         direcao_y = (self.world_y - (self.direcao.y / 2)) // 1
 
         return direcao_x, direcao_y
 
-    def updateDirection(self):
-        direcao_x, direcao_y = self.getRear()
+    def update_direction(self):
+        direcao_x, direcao_y = self.get_rear()
+        sucesso = self.path_find(direcao_x, direcao_y)
 
-        # print(direcao_x)
-        # print(direcao_y)
-
-        redor = self.caminho.getSurrounding((direcao_x, direcao_y))
-
-        # print(redor)
-
-        if redor['up'] == 'space':
-            self.direcao = self.up.normalized()
-            self.is_moving = True
-            self.override_human = True
-            self.history.append('up')
-        elif redor['right'] == 'space':
-            self.direcao = self.right.normalized()
-            self.is_moving = True
-            self.override_human = True
-            self.history.append('right')
-        elif redor['down'] == 'space':
-            self.direcao = self.down.normalized()
-            self.is_moving = True
-            self.override_human = True
-            self.history.append('down')
-        elif redor['left'] == 'space':
-            self.direcao = self.left.normalized()
-            self.is_moving = True
-            self.override_human = True
-            self.history.append('left')
-        else:
-            ultima_direcao = self.history.pop()
-
-            # print(ultima_direcao)
-
-            if ultima_direcao == 'down':
-                self.direcao = self.up.normalized()
-                self.is_moving = True
-                self.override_human = True
-            elif ultima_direcao == 'left':
-                self.direcao = self.right.normalized()
-                self.is_moving = True
-                self.override_human = True
-            elif ultima_direcao == 'up':
-                self.direcao = self.down.normalized()
-                self.is_moving = True
-                self.override_human = True
-            elif ultima_direcao == 'right':
-                self.direcao = self.left.normalized()
-                self.is_moving = True
-                self.override_human = True
+        if not sucesso:
+            print('Erro de pathfind!')
 
         if self.posicao_objetivo:
             self.direcao = Vec3(
@@ -150,29 +103,74 @@ class Robo(Entity):
                 + self.right * (held_keys['d'] - held_keys['a'])
             ).normalized()
 
+    def path_find(self, direcao_x, direcao_y):
+        # print(direcao_x)
+        # print(direcao_y)
+
+        redor = self.caminho.getSurrounding((direcao_x, direcao_y))
+
+        # print(redor)
+
+        if redor['up'] == 'space':
+            self.direcao = self.up.normalized()
+            self.historia.append(('up', self.delta))
+        elif redor['right'] == 'space':
+            self.direcao = self.right.normalized()
+            self.historia.append(('right', self.delta))
+        elif redor['down'] == 'space':
+            self.direcao = self.down.normalized()
+            self.historia.append(('down', self.delta))
+        elif redor['left'] == 'space':
+            self.direcao = self.left.normalized()
+            self.historia.append(('left', self.delta))
+        else:
+            if len(self.historia):
+                ultima_direcao, delta = self.historia.pop()
+
+                # print(ultima_direcao)
+
+                if ultima_direcao == 'down':
+                    self.direcao = self.up.normalized()
+                elif ultima_direcao == 'left':
+                    self.direcao = self.right.normalized()
+                elif ultima_direcao == 'up':
+                    self.direcao = self.down.normalized()
+                elif ultima_direcao == 'right':
+                    self.direcao = self.left.normalized()
+                else:
+                    return False
+
+                self.delta = delta
+            else:
+                return False
+
+        return True
+
     def memorizeHitinfo(self, hit_info: HitInfo):
         for entity in hit_info.entities:
             self.caminho.memorizeObstacle(
                 (entity.world_x // 1, entity.world_y // 1))
 
     def memorizePath(self):
-        direcao_x, direcao_y = self.getRear()
+        direcao_x, direcao_y = self.get_rear()
 
         self.caminho.memorizePath((direcao_x, direcao_y))
 
     def update(self):
+        self.delta = time.dt
+
         if self.espera > 0:
-            self.espera -= time.dt
+            self.espera -= self.delta
         else:
-            self.findObjective()
+            self.find_objective()
 
             visao = self.see()
 
             if visao:
                 self.memorizeHitinfo(visao)
 
-            self.updateDirection()
+            self.update_direction()
 
-            if self.canMove() and self.isMoving():
+            if self.canMove():
                 self.move()
                 self.memorizePath()
